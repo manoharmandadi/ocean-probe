0e6636cfe10873b703df9e8b84da569b9828b625
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 18:25:10 +0530

    Update test to use Coordinate.

--- src/test/java/com/maverick/probe/component/FloorGridTest.java
+++ src/test/java/com/maverick/probe/component/FloorGridTest.java
@@ -1,5 +1,6 @@
 package com.maverick.probe.component;
 
+import com.maverick.probe.model.Coordiante;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +13,6 @@ public class FloorGridTest {
 
     @Test
     public void testGridInitialization(){
-        Assertions.assertNotNull(grid.getElement(0,0));
+        Assertions.assertNotNull(grid.getElement(new Coordiante(0, 0)));
     }
 }

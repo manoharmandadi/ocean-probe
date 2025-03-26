eee788dbdd43aeae95554de249196ef79bfc0146
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 20:16:48 +0530

    Implement getCurrentPosition()

--- src/main/java/com/maverick/probe/controller/ProbeController.java
+++ src/main/java/com/maverick/probe/controller/ProbeController.java
@@ -1,7 +1,10 @@
 package com.maverick.probe.controller;
 
+import com.maverick.probe.model.Coordiante;
+import com.maverick.probe.service.NavigationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,11 +14,20 @@ public class ProbeController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProbeController.class);
 
+    @Autowired
+    private NavigationService navigationService;
+
     @PostMapping("/move")
     @ResponseStatus(HttpStatus.CREATED)
     public void move(@RequestParam(value = "forward", defaultValue = "true") boolean forward){
         logger.debug("Invoked move command, Forward: "+forward);
 
+    }
+
+    @GetMapping("/position")
+    @ResponseStatus(HttpStatus.OK)
+    public Coordiante getPosition(){
+        return navigationService.getCurrentPosition();
     }
 
 }

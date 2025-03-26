64ac54548de9501b788f5ca912764fa02733b931
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 17:38:19 +0530

    Initial commit

---
+++ src/main/java/com/maverick/probe/OceanProbeApplication.java
@@ -0,0 +1,13 @@
+package com.maverick.probe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanProbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanProbeApplication.class, args);
+	}
+
+}

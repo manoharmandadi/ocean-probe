82c017b18d04ea56588688f3babb06c8462506fc
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 19:50:34 +0530

    Implement getNextCoordinate()

--- src/main/java/com/maverick/probe/service/NavigationServiceImpl.java
+++ src/main/java/com/maverick/probe/service/NavigationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.maverick.probe.service;
 
 import com.maverick.probe.model.Coordiante;
+import com.maverick.probe.model.Direction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,10 +18,26 @@ public class NavigationServiceImpl implements NavigationService{
     @Value("${probe.initial.y}")
     private int y;
 
+    @Value("${probe.initial.direction}")
+    private Direction direction;
+
     @Override
     public Coordiante move(boolean forward) {
-        logger.debug("Moving from "+x+" ,"+y);
+        logger.debug("Moving "+(forward? "forward": "backward")+ ". Facing :"+direction);
 
         return new Coordiante(0, 0);
+    }
+
+    public Coordiante getNextCoordinate(boolean forward){
+        int newX = x;
+        int newY = y;
+        int move =  (forward)? 1: -1;
+        switch (direction){
+            case SOUTH -> newY = y - move;
+            case WEST -> newX = x - move;
+            case NORTH -> newY = y + move;
+            case EAST -> newX = x + move;
+        }
+        return new Coordiante(newX, newY);
     }
 }

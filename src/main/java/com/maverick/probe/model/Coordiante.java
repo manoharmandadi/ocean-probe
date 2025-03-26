95f99d6ffd009af7e23c2ca11d53ee13e484ff7a
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 20:03:18 +0530

    Remove @Data from Lombok(not working) and implement Getters, Equal and hashCode

--- src/main/java/com/maverick/probe/model/Coordiante.java
+++ src/main/java/com/maverick/probe/model/Coordiante.java
@@ -1,8 +1,7 @@
 package com.maverick.probe.model;
 
-import lombok.Data;
+import java.util.Objects;
 
-@Data
 public class Coordiante {
 
     private int x;
@@ -13,5 +12,25 @@ public class Coordiante {
         this.x = x;
         this.y = y;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Coordiante that)) return false;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
 
 }

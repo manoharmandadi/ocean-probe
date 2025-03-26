3cf22df7a27cb4a2a2c233ea24745f7e1b54731d
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 17:45:15 +0530

    Add test for Move Forward

---
+++ src/test/java/com/maverick/probe/controller/ProbeControllerTest.java
@@ -0,0 +1,26 @@
+package com.maverick.probe.controller;
+
+import com.maverick.probe.OceanProbeApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.MOCK, classes = OceanProbeApplication.class)
+@AutoConfigureMockMvc
+public class ProbeControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+
+    @Test
+    public void testMoveForward() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/navigate/move"))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+}

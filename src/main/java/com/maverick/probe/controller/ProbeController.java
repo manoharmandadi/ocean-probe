31674f6c4c59d4c1696ecbf04e1445b34b79d920
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 18:23:48 +0530

    Add logger

--- src/main/java/com/maverick/probe/controller/ProbeController.java
+++ src/main/java/com/maverick/probe/controller/ProbeController.java
@@ -1,5 +1,7 @@
 package com.maverick.probe.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/navigate")
 public class ProbeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProbeController.class);
+
     @PostMapping("/move")
     @ResponseStatus(HttpStatus.CREATED)
     public void move(@RequestParam(value = "forward", defaultValue = "true") boolean forward){
+        logger.debug("Invoked move command, Forward: "+forward);
 
     }
 

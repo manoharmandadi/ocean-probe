6115954b27572503f2c374e8018f225ce052da9a
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 17:59:32 +0530

    Empty test for FloorGrid.

---
+++ src/test/java/com/maverick/probe/component/FloorGridTest.java
@@ -0,0 +1,14 @@
+package com.maverick.probe.component;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class FloorGridTest {
+
+//    FloorGrid grid;
+
+    public void testGridInitialization(){
+//        grid.
+    }
+}

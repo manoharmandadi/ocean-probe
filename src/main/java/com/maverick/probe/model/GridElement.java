397fcc2f39f635ae14ea15c48d8ded0f09ddf808
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 17:54:43 +0530

    Add Grid Element. Represents a given coordinate x,y on Ocean Floor.

---
+++ src/main/java/com/maverick/probe/model/GridElement.java
@@ -0,0 +1,13 @@
+package com.maverick.probe.model;
+
+import lombok.Data;
+
+@Data
+public class GridElement {
+
+    private boolean isNavigable;
+
+    //Incase Floor is used by multiple probes
+    //private boolean isOccupied;
+
+}

4321ab82426889ecd18dd1509bad025afe6a6950
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 20:42:54 +0530

    Add turn()

--- src/main/java/com/maverick/probe/service/NavigationService.java
+++ src/main/java/com/maverick/probe/service/NavigationService.java
@@ -1,11 +1,14 @@
 package com.maverick.probe.service;
 
 import com.maverick.probe.model.Coordiante;
+import com.maverick.probe.model.Direction;
 
 public interface NavigationService {
 
     //Return new Cooridates of the probe after move.
     Coordiante move(boolean forward);
+
+    Direction turn(boolean left);
 
     //Return new Coordinates for move operation
     Coordiante getNextCoordinate(boolean forward);

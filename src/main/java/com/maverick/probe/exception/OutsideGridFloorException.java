786d9b08ee888b0317ff9d85bbe10dbfb00a4aeb
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-26 20:26:13 +0530

    Add Exceptions for UnNavigablePosition and OutsideGridFloor.

---
+++ src/main/java/com/maverick/probe/exception/OutsideGridFloorException.java
@@ -0,0 +1,8 @@
+package com.maverick.probe.exception;
+
+public class OutsideGridFloorException extends RuntimeException{
+
+    public OutsideGridFloorException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/maverick/probe/exception/UnNavigablePositionException.java
@@ -0,0 +1,8 @@
+package com.maverick.probe.exception;
+
+public class UnNavigablePositionException extends RuntimeException {
+
+    public UnNavigablePositionException(String message){
+        super(message);
+    }
+}
